cf27f5ba553b3fa571a2822b51f0c02d98c57061
Tanzim Khan <tkha0014@student.monash.edu>
2024-03-31 09:45:18 +1100

    Initial Commit

---
+++ Tripzo -Travel Planner Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tripzo -Travel Planner Application
+//
+//  Created by Tanzim Islam Khan on 31/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
