dcda06d9ab37ee2d9e555fae1f6a5c99ea530e86
Tanzim Khan <dihan2468@gmail.com>
2024-06-08 20:32:57 +1000

    Integrated Create Trip Functionality for the User where they add itineraries and then it adds them to the home screen

---
+++ Tripzo -Travel Planner Application/Home Screen/AddTripsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AddTripsCollectionViewCell.swift
+//  Tripzo -Travel Planner Application
+//
+//  Created by Tanzim Islam Khan on 8/6/2024.
+//
+
+import UIKit
+
+class AddTripsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Tripzo -Travel Planner Application/Models/Itinerary.swift
@@ -0,0 +1,8 @@
+//
+//  Itinerary.swift
+//  Tripzo -Travel Planner Application
+//
+//  Created by Tanzim Islam Khan on 8/6/2024.
+//
+
+import Foundation
